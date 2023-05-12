a402318ae0c3827b1575fac7d5f146e2bb0c0d1c
bhautikmaniya86 <126137976+bhautikmaniya86@users.noreply.github.com>
2023-05-13 01:07:21 +0530

    Update Note.tsx

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import { Notes } from "../../pages/Notes/Notes";
 import { RemoveRedEye, Delete, Edit } from "@mui/icons-material";
 
